8bb5aab90b9de17c1d1e45081efde0651116c778
Sofia-Cardozo <sofia11cardozo@gmail.com>
2025-07-29 17:49:20 -0300

    De public a raiz models

--- public/routes/auth.js
+++ public/routes/auth.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 const path = require('path');
-const User = require(path.join(__dirname, '..', 'models', 'User'));
+const User = require(path.join(__dirname, '..', '..', 'models', 'User'));
 // …
 
 
